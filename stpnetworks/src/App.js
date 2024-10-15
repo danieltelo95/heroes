1832a061bbb6999c205a36a0ae836cc5c381417c
danieltelo95 <daniel.qui.tello@gmail.com>
2024-10-15 01:06:32 -0500

    Footer

--- stpnetworks/src/App.js
+++ stpnetworks/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './index.css';
 import Banner from './components/Banner';
 import HeroGrid from './components/HeroGrid';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <HeroGrid />
+      <Footer />
     </div>
   );
 }
---
+++ stpnetworks/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+
+    return(
+        <div className="text-black text-sm font-montserrat flex justify-start ml-20">
+            <nav className="flex space-x-4">
+                <a>Términos y condiciones</a>
+                <a>Política de tratamiento de datos</a>
+                <a>Contáctenos </a>
+            </nav>
+        </div>
+    )
+
+}
+
+export default Footer;
